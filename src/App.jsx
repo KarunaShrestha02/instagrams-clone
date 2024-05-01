b8df72f38effdd1c614952697ebef2f88b98e82b
KarunaShrestha02 <168328218+KarunaShrestha02@users.noreply.github.com>
2024-05-01 20:59:50 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './page/HomePage/HomePage';
+import AuthPage from './page/AuthPage/AuthPage';
+import PageLayout from './Layouts/PageLayout/PageLayout';
+
+
+function App() {
+
+
+  return (
+    <PageLayout>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/auth' element={<AuthPage />} />
+      </Routes>
+    </PageLayout>
+  );
+}
+
+export default App;
