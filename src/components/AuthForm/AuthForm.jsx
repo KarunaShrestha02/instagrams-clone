867dff4c24aff35b594273b66ffa8ba9ba74d527
KarunaShrestha02 <karuna100502@gmail.com>
2024-05-03 16:38:28 +0545

    added authentication features

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,53 +1,23 @@
 import { Box, Button, Flex, Image, Input, Text, VStack } from "@chakra-ui/react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
+
 
 
 const AuthForm = () => {
     const [isLogin, setIsLogin]= useState(true);
-    const navigate=useNavigate();
-    const [inputs,setInputs]= useState({
-        email:'',
-        password:'',
-        confirmPassword:''
-    });
-    const handleAuth =()=> {
-        if(!inputs.email || !inputs.password  )
-        {
-            alert("Please fill all the fields");
-            return;
-        }
-        navigate("/");
-    };
+    
+   
+   
   return  (
   <>
  <Box border={"1px solid gray"} borderRadius={4} padding={5}>
     <VStack spacing={4}>
         <Image src="/logo (1).png" h={24} cursor={"pointer"} alt="Instagram" />
-       <Input
-        placeholder="Email"
-        fontSize={14}
-         type='email'
-       value={inputs.email}
-       onChange={(e)=> setInputs({...inputs,email:e.target.value})}
-       />
-        <Input
-         placeholder="Password"
-         fontSize={14} 
-          type='password'
-        value={inputs.password}
-        onChange={(e)=> setInputs({...inputs,password:e.target.value})}
-        
-        />
-        {!isLogin ? <Input
-         placeholder="Confirm Password"
-        value={inputs.confirmPassword}
-        onChange={(e)=> setInputs({...inputs,confirmPassword:e.target.value})}
-       fontSize={14} 
-       type="password"/>:null}
-        <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14} onClick={handleAuth}>
-            {isLogin? "Log In" : "Sign Up"}
-            </Button> 
+      
+       
+       {isLogin ? <Login/> : <Signup/>}
 
             {/*---------------OR--------------- */}
             <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
