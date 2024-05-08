8bdd4073efd3824c00b3b520b695071931caffed
KarunaShrestha02 <karuna100502@gmail.com>
2024-05-08 21:25:30 +0545

    updated uselogin hooks

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import HomePage from './page/HomePage/HomePage';
 import AuthPage from './page/AuthPage/AuthPage';
 import PageLayout from './Layouts/PageLayout/PageLayout';
 import ProfilePage from './page/ProfilePage/ProfilePage';
-
-
-
-import useAuthStore from './store/authStore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase/firebase';
 
 
 function App() {
 
-const authUser= useAuthStore(state=> state.user);
+const {authUser}= useAuthState(auth);
   return (
     <PageLayout>
     <Routes>
---
+++ src/hooks/useLogin.jsx
@@ -0,0 +1,8 @@
+
+import useShowToast from './useShowToast'
+
+const useLogin = () => {
+  const showToast= useShowToast();
+}
+
+export default useLogin;
