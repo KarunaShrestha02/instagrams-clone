8d86fa86888fd50a753ceaf1d803bcc7775a5189
KarunaShrestha02 <karuna100502@gmail.com>
2024-05-10 13:57:52 +0545

    restrictions if account already exists while signing in

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -3,8 +3,7 @@ import { auth, firestore } from "../../firebase/firebase";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import useShowToast from "../../hooks/useShowToast";
 import useAuthStore from "../../store/authStore";
-import { doc, setDoc } from "firebase/firestore";
-
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 const GoogleAuth = ({prefix}) => {
   const [signInWithGoogle,  error] = useSignInWithGoogle(auth);
@@ -18,10 +17,20 @@ const GoogleAuth = ({prefix}) => {
         if(!newUser && error)
           {
             showToast("Error",error.message,"error");
-            return 
+            return; 
           }
-          if(newUser)
+          const userRef = doc(firestore, "users", newUser.user.uid);
+          const userSnap = await getDoc(userRef);
+          if(userSnap.exists())
+            //login
             {
+                const userDoc =userSnap.data();
+                localStorage.setItem("user-info",JSON.stringify(userDoc));\
+                loginUser(userDoc);
+            }
+          else
+            {
+              //signup
             const userDoc = {
               uid:newUser.user.uid,
               email:newUser.user.email,
@@ -33,7 +42,7 @@ const GoogleAuth = ({prefix}) => {
               following:[],
               posts:[],
               createdAt:Date.now()
-            }
+            };
             await setDoc(doc(firestore, "users",newUser.user.uid), userDoc);
             localStorage.setItem("user-info", JSON.stringify(userDoc));
             loginUser(userDoc);
@@ -42,9 +51,7 @@ const GoogleAuth = ({prefix}) => {
       catch(error)
       {
         showToast("Error", error.message,"error");
-    
-
-
+     
     }
   }
 
