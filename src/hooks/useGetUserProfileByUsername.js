7efee3d00421509696192f27ac4e5a0f005ad3e7
KarunaShrestha02 <karuna100502@gmail.com>
2024-05-14 15:47:48 +0200

    no message

--- src/hooks/useGetUserProfileByUsername.js
+++ src/hooks/useGetUserProfileByUsername.js
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import useShowToast from './useShowToast';
-import { collection, getDocs, query, where } from 'firebase/firestore';
-import { firestore } from '../firebase/firebase';
+import { useEffect, useState } from "react";
+import useShowToast from "./useShowToast";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
+import useUserProfileStore from "../store/userProfileStore";
 
-const useGetUserProfileByUsername = () => {
-  const [isLoading,setIsLoading] =useState(true);
-  const showToast=useShowToast;
+const useGetUserProfileByUsername = (username) => {
+	const [isLoading, setIsLoading] = useState(true);
+	const showToast = useShowToast();
+	const { userProfile, setUserProfile } = useUserProfileStore();
 
-  useEffect(() =>{
-    const getUserProfile = async()=> {
-      setIsLoading(true)
-      try{
-        const g=query(collection(firestore,"users"),where("username","==",username));
-        const querySnapshot =await getDocs(q);
+	useEffect(() => {
+		const getUserProfile = async () => {
+			setIsLoading(true);
+			try {
+				const q = query(collection(firestore, "users"), where("username", "==", username));
+				const querySnapshot = await getDocs(q);
 
-        if(querySnapshot.empty) return setUserP(null);
+				if (querySnapshot.empty) return setUserProfile(null);
 
-      }
-      catch(error)
-      {
-        showToast('Error',error.message,'error');
-      }
-    }
-      getUserProfile()
-  },[])
+				let userDoc;
+				querySnapshot.forEach((doc) => {
+					userDoc = doc.data();
+				});
 
-  }
- 
+				setUserProfile(userDoc);
+				console.log(userDoc);
+			} catch (error) {
+				showToast("Error", error.message, "error");
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		getUserProfile();
+	}, [setUserProfile, username, showToast]);
+
+	return { isLoading, userProfile };
+};
 
 export default useGetUserProfileByUsername;
