8c674fe1e8a19a6d2d192123dd0bca07f1ff8225
KarunaShrestha02 <karuna100502@gmail.com>
2024-05-16 22:05:35 +0545

    no message

--- src/hooks/usePreviewImg.js
+++ src/hooks/usePreviewImg.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState } from "react";
+import useShowToast from "./useShowToast";
 
 const usePreviewImg = () => {
-  return (
-    <div>usePreviewImg</div>
-  )
-}
+	const [selectedFile, setSelectedFile] = useState(null);
+	const showToast = useShowToast();
+	const maxFileSizeInBytes = 2 * 1024 * 1024; // 2MB
 
-export default usePreviewImg
+	const handleImageChange = (e) => {
+		const file = e.target.files[0];
+		if (file && file.type.startsWith("image/")) {
+			if (file.size > maxFileSizeInBytes) {
+				showToast("Error", "File size must be less than 2MB", "error");
+				setSelectedFile(null);
+				return;
+			}
+			const reader = new FileReader();
+
+			reader.onloadend = () => {
+				setSelectedFile(reader.result);
+			};
+
+			reader.readAsDataURL(file);
+		} else {
+			showToast("Error", "Please select an image file", "error");
+			setSelectedFile(null);
+		}
+	};
+
+	return { selectedFile, handleImageChange, setSelectedFile };
+};
+
+export default usePreviewImg;
