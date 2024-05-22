e5e98e0e2131de43dfc2aa7603ff26c043afe264
KarunaShrestha02 <karuna100502@gmail.com>
2024-05-22 23:12:26 +0545

    no message

--- src/components/Sidebar/SidebarItems.jsx
+++ src/components/Sidebar/SidebarItems.jsx
@@ -3,12 +3,13 @@ import Home from './Home'
 import Notifications from './Notifications'
 import CreatePost from './CreatePost'
 import ProfileLink from './ProfileLink'
+import Search from './Search'
 
 const SidebarItems = () => {
   return (
     <>
     <Home />
-    {/*Search */}
+    <Search />
     <Notifications />
     <CreatePost />
     <ProfileLink/>
