814c0fb47510b0c967a308328cc451b7e5b60c04
KarunaShrestha02 <karuna100502@gmail.com>
2024-05-23 21:24:12 +0545

    no message

--- src/hooks/useSearchUser.js
+++ src/hooks/useSearchUser.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState } from "react";
+import useShowToast from "./useShowToast";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
 
 const useSearchUser = () => {
-  return (
-    <div>useSearchUser</div>
-  )
-}
+	const [isLoading, setIsLoading] = useState(false);
+	const [user, setUser] = useState(null);
+	const showToast = useShowToast();
 
-export default useSearchUser
+	const getUserProfile = async (username) => {
+		setIsLoading(true);
+		setUser(null);
+		try {
+			const q = query(collection(firestore, "users"), where("username", "==", username));
+
+			const querySnapshot = await getDocs(q);
+			if (querySnapshot.empty) return showToast("Error", "User not found", "error");
+
+			querySnapshot.forEach((doc) => {
+				setUser(doc.data());
+			});
+		} catch (error) {
+			showToast("Error", error.message, "error");
+			setUser(null);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return { isLoading, getUserProfile, user, setUser };
+};
+
+export default useSearchUser;
