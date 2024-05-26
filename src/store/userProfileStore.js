8a5d55220a9600454242d543e49755874ee6fc4f
KarunaShrestha02 <karuna100502@gmail.com>
2024-05-26 21:05:13 +0545

    no message

--- src/store/userProfileStore.js
+++ src/store/userProfileStore.js
@@ -3,8 +3,10 @@ import {create} from "zustand";
 const useUserProfileStore = create((set) => ({
   userProfile:null,
   setUserProfile:(userProfile)=> set({userProfile}),
-    //addpost()
+    addpost: (post) => set(state => ({
+      userProfile:{...state.userProfile,ppsts:[post.id,...state.user.userProfile.posts]}
+    }))
   
-}))
+}));
 
 export default useUserProfileStore;
