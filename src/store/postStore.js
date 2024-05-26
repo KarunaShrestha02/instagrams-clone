57726dbc30dee287e513f4781680bd7fd2b76533
KarunaShrestha02 <karuna100502@gmail.com>
2024-05-26 21:00:29 +0545

    no message

--- src/store/postStore.js
+++ src/store/postStore.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import { create } from "zustand";
 
-const postStore = () => {
-  return (
-    <div>postStore</div>
-  )
-}
-
-export default postStore
+const usePostStore = create((set) => ({
+    posts:[],
+    createPost :(post) => set((state) => ({ posts : [post, ...state.posts]})),
+    //deletePost
+    //addComment
+    //setPosts
+}));
+export default usePostStore;
