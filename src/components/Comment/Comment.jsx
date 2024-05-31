d416fcdc7201ad8e1ab9d9e3ee15acf5fd15f94b
KarunaShrestha02 <karuna100502@gmail.com>
2024-05-31 20:27:55 +0545

    no message

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,19 +1,22 @@
-import { Avatar, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Flex, Skeleton, SkeletonCircle, Text } from "@chakra-ui/react";
+import useGetUserProfileById from "../../hooks/useGetUserProfileById";
 
 
 const Comment = ({createdAt, username, profilePic, text}) => {
+    const {userProfile,isLoading}= useGetUserProfileById(comment.createdBy)
+    if(isLoading) return <CommentSkeleton />
   return (
     <Flex gap={4}>
-        <Avatar src={profilePic} name={username} size={"sm"}/>
+        <Avatar src={userProfile.profilePicURL}  size={"sm"}/>
         <Flex direction={"column"}>
             <Flex gap={2}>
                 <Text fontWeight={"bold"} fontSize={12}>
-                    {username}
+                    {userProfile.username}
                 </Text>
-                <Text fontSize={14}>{text}</Text>
+                <Text fontSize={14}>{comment.comment}</Text>
             </Flex>
             <Text fontSize={12} color={"gray"}>
-                {createdAt}
+                {/*createdAt*/}
             </Text>
         </Flex>
     </Flex>
@@ -21,3 +24,17 @@ const Comment = ({createdAt, username, profilePic, text}) => {
 };
 
 export default Comment;
+const CommentSkeleton=()=>{
+    return(
+    <Flex gap={4} w={"full"} alignItems={"center"}>
+        <SkeletonCircle h={10} w='10' />
+        <Flex gap={1} flexDir={"column"}>
+            <Skeleton height={2} width={100}/>
+            <Skeleton height={2} width={50}/>
+
+            
+        </Flex>
+
+    </Flex>
+    );
+};
