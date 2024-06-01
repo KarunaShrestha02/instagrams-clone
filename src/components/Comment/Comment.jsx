a03eaaa1c2fdbb79a474a75f6d7b42402f30b518
KarunaShrestha02 <karuna100502@gmail.com>
2024-06-01 18:31:12 +0545

    no message

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,5 +1,6 @@
-import { Avatar, Flex, Skeleton, SkeletonCircle, Text } from "@chakra-ui/react";
+import { Avatar, Flex,  Skeleton, SkeletonCircle, Text } from "@chakra-ui/react";
 import useGetUserProfileById from "../../hooks/useGetUserProfileById";
+import { Link } from "react-router-dom";
 
 
 const Comment = ({createdAt, username, profilePic, text}) => {
@@ -7,12 +8,16 @@ const Comment = ({createdAt, username, profilePic, text}) => {
     if(isLoading) return <CommentSkeleton />
   return (
     <Flex gap={4}>
+        <Link to={'/${userProfile.username}'}>
         <Avatar src={userProfile.profilePicURL}  size={"sm"}/>
+        </Link>
         <Flex direction={"column"}>
-            <Flex gap={2}>
+            <Flex gap={2} alignItems={"center"}>
+            <Link to={'/${userProfile.username}'}>
                 <Text fontWeight={"bold"} fontSize={12}>
                     {userProfile.username}
                 </Text>
+                </Link>
                 <Text fontSize={14}>{comment.comment}</Text>
             </Flex>
             <Text fontSize={12} color={"gray"}>
