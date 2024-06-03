560b1e1570a4a51336250325267b4ad4851f32bc
KarunaShrestha02 <karuna100502@gmail.com>
2024-06-03 16:31:13 +0200

    no message

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -5,26 +5,24 @@ import usePostComment from '../../hooks/usePostComment';
 import useAuthStore from '../../store/authStore';
 
 const PostFooter = ({username,isprofilePage}) => {
-    const [liked, setLiked]= useState(false);
-    const[likes, setLikes]= useState(1000);
+  
     const {isCommenting,handlePostComment}= usePostComment();
     const [comment,setComment]=useState("");
     const authUser=useAuthStore(state=> state.user);
     const commentRef=useRef(null);
+    const {handleLikePost,isLiked,likes}=useLikePost(post);
 
     const handleSubmitComment =async ()=>{
         await handlePostComment(post.id,comment);
         setComment("");
         
     }
-    const handleLike =()=> {
-        
-    };
+    
   return (
     <Box mb={10} marginTop={"auto"}>
     <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
-        <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
-            {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
+        <Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
+            {!isLiked? <NotificationsLogo /> : <UnlikeLogo />}
         </Box>
         <Box cursor={"pointer"} fontSize={18} onClick={()=> commentRef.current.focus()}>
             <CommentLogo />
