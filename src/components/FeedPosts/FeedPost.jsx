46ebc60cc8dc3098fecf6063ee5e8a3c77b79ad6
KarunaShrestha02 <karuna100502@gmail.com>
2024-06-04 19:56:29 +0545

    no message

--- src/components/FeedPosts/FeedPost.jsx
+++ src/components/FeedPosts/FeedPost.jsx
@@ -1,12 +1,14 @@
 import { Box, Image } from "@chakra-ui/react";
 import PostHeader from "./PostHeader";
 import PostFooter from "./PostFooter";
+import useGetUserProfileById from '../../hooks/useGetUserProfileById';
 
 
 const FeedPost = ({post}) => {
+  const {userProfile}=useGetUserProfileById(post.createdBy);
   return (
     <>
-   { /*<PostHeader post={post}  />*/}
+   <PostHeader post={post}  creatorProfile={userProfile}/>
     <Box my={2} borderRadius={4}
     overflow={"hidden"}> 
         <Image src={post.imgURL} alt={"FEED POST IMG"} />
--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -1,13 +1,13 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
 
-const PostHeader = ({post}) => {
+const PostHeader = ({post, creatorProfile}) => {
   return( 
 <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
     <Flex alignItems={"center"} gap={2}>
-        <Avatar src={avatar} alt='user profile pic' size={"sm"} />
+        <Avatar src={creatorProfile.profilePicURL} alt='user profile pic' size={"sm"} />
         <Flex fontSize={12} fontWeight={"bold"} gap='2'>
-           {username} 
+           {creatorProfile.username} 
             <Box color={"gray.500"}>. 1w</Box>
             
         </Flex>
